e0449715a9234c9e4ccc27eb79aa00a18b264d27
codingXllama <osamaalchalabi2@gmail.com>
2020-08-14 23:15:49 -0400

    added something

---
+++ 1.py
@@ -0,0 +1,14 @@
+# def main():
+#   print("The answer is", magic(5))
+# def magic(num):
+#   answer = num + 2 * 10
+#   return answer
+# main()
+
+
+def test(boolean):
+  return True if boolean=="true" else False
+
+
+
+print(test("terue"))
